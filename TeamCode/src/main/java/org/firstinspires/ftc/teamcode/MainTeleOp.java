c7b6c27256d73133ac0d983ff23d9830e31fdf78
valerie <valerie@ouppy.gay>
2026-07-23 14:22:20 +1000

    refactors
    - main.java -> MainTeleOp.java
    - Seperate into more methods
    - Add Ivy dependency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -7,36 +7,31 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import java.util.function.Supplier;
 
 @TeleOp
-public class main extends OpMode {
+public class MainTeleOp extends OpMode {
+    private static final Pose startingPose = new Pose();
+    private static final boolean ROBOT_CENTRIC = true;
 
+    double driveSpeedScale = 1.0;
+    boolean lastOptionState = false;
     /* ================================
       Auto / Drive
       (See PedroPathing documentation)
     ================================ */
     private Follower follower;
-    private static final Pose startingPose = new Pose();
     private Supplier<PathChain> pathChain;
     // In automated driving mode.
     private boolean automated = false;
-
-
     /* ==========================
       Telemetry
       (See Panels documentation)
     ========================== */
     private TelemetryManager telemetryM;
-
-
-    private static final boolean ROBOT_CENTRIC = true;
-    double driveSpeedScale = 1.0;
-    boolean lastOptionState = false;
 
     @Override
     public void init() {
@@ -57,16 +52,11 @@ public class main extends OpMode {
     @Override
     public void loop() {
         follower.update();
+        setDriveSpeedScale();
 
         double forward = -gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
-
-        boolean currentOptionState = gamepad1.options;
-        if (currentOptionState && !lastOptionState) {
-            driveSpeedScale = (driveSpeedScale == 1.0) ? 0.5 : 1.0;
-        }
-        lastOptionState = currentOptionState;
 
         if (!automated) {
             follower.setTeleOpDrive(
@@ -78,13 +68,38 @@ public class main extends OpMode {
         }
 
         if (gamepad1.aWasPressed()) {
-            follower.followPath(pathChain.get());
-            automated = true;
+            startAutoDrive();
         }
 
+        if (automated && (gamepad1.bWasPressed() || !follower.isBusy())) {
+            stopAutoDrive();
+        }
+
+        telemetry();
+    }
+
+    private void setDriveSpeedScale() {
+        boolean currentOptionState = gamepad1.options;
+        if (currentOptionState && !lastOptionState) {
+            driveSpeedScale = (driveSpeedScale == 1.0) ? 0.5 : 1.0;
+        }
+        lastOptionState = currentOptionState;
+    }
+
+    private void startAutoDrive() {
+        follower.followPath(pathChain.get());
+        automated = true;
+    }
+
+    private void stopAutoDrive() {
+        follower.startTeleopDrive();
+        automated = false;
+    }
+
+    private void telemetry() {
         telemetryM.addData("Position", follower.getPose());
-        telemetryM.addData("velocity", follower.getVelocity());
-        telemetryM.addData("automated", automated);
+        telemetryM.addData("Velocity", follower.getVelocity());
+        telemetryM.addData("Automated", automated);
         telemetryM.update(telemetry);
     }
 }
